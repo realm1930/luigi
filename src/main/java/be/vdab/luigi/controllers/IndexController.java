23c5b3ac07a1ecb64d69b9bffd3d4cc360c62ee9
Arne <a.vaneycken@gmail.com>
2020-10-13 10:13:51 +0200

    initial

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,18 +1,19 @@
 package be.vdab.luigi.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 import java.time.LocalTime;
 
-@RestController
+@Controller
 @RequestMapping("/")
 class IndexController {
     @GetMapping
-    public String index(){
+    public ModelAndView index(){
         var morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
-        return "<!doctype html><html><title>Hallo</title><body>Goede "
-                +morgenOfMiddag+"</body></html>";
+        return new ModelAndView("index","moment",morgenOfMiddag);
     }
 }
