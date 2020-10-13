3c466dfc72e865854bea5603359f36fb0765a598
Arne <a.vaneycken@gmail.com>
2020-10-13 13:50:14 +0200

    domain

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -3,10 +3,12 @@ package be.vdab.luigi.controllers;
 import be.vdab.luigi.domain.Pizza;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("pizzas")
@@ -15,9 +17,18 @@ public class PizzaController {
             new Pizza(1, "Prosciutto", BigDecimal.valueOf(4), true),
             new Pizza(2, "Margherita", BigDecimal.valueOf(5), false),
             new Pizza(3, "Calzone", BigDecimal.valueOf(4), false)
-};
-@GetMapping
-public ModelAndView pizzas(){
-        return new ModelAndView("pizzas","pizzas",pizzas);
-        }
-        }
+    };
+
+    @GetMapping
+    public ModelAndView pizzas() {
+        return new ModelAndView("pizzas", "pizzas", pizzas);
+    }
+
+    @GetMapping("{id}")
+    public ModelAndView pizza(@PathVariable long id){
+        var modelAndView = new ModelAndView("pizza");
+        Arrays.stream(pizzas).filter(pizza->pizza.getId()==id).findFirst().ifPresent(
+                pizza -> modelAndView.addObject(pizza));
+        return modelAndView;
+    }
+}
