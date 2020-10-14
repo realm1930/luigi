853f58df8247ed33ff5a28b5245790a5c83d80f6
Arne <a.vaneycken@gmail.com>
2020-10-14 09:57:34 +0200

    domain

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -9,16 +9,19 @@ import be.vdab.luigi.domain.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 @RequestMapping("/")
 class IndexController {
+    private final AtomicInteger aantalKerenBekeken = new AtomicInteger();
     @GetMapping
     public ModelAndView index(){
         var morgenOfMiddag = LocalTime.now().getHour() < 12 ? "morgen" : "middag";
         var modelAndView = new ModelAndView("index","moment",morgenOfMiddag);
         modelAndView.addObject("zaakvoerder",new Persoon("Luigi", "Peperone", 7, true, LocalDate.of(1966, 1, 31),
         new Adres("Grote markt", "3", 9700, "Oudenaarde")));
+        modelAndView.addObject("aantalKeerBekeken", aantalKerenBekeken.incrementAndGet());
         return modelAndView;
     }
 }
