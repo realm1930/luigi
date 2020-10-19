f9a5edce8ec43f2b3cd504299f1dd3b6933f2376
Arne <a.vaneycken@gmail.com>
2020-10-19 12:48:38 +0200

    application.properties

--- src/test/java/be/vdab/luigi/repositories/DataSourceTest.java
+++ src/test/java/be/vdab/luigi/repositories/DataSourceTest.java
@@ -1,10 +1,7 @@
 package be.vdab.luigi.repositories;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-
 import javax.sql.DataSource;
-
 import java.sql.SQLException;
 
 @JdbcTest
