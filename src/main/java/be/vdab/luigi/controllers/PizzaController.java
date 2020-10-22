e5ec4d856d14c8a8ceafc9ff69c2253e02cb2a9b
Arne <a.vaneycken@gmail.com>
2020-10-22 16:49:06 +0200

    validation messages

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -7,14 +7,17 @@ import be.vdab.luigi.exceptions.KoersClientException;
 import be.vdab.luigi.forms.VanTotPrijsForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
@@ -27,10 +30,12 @@ public class PizzaController {
     private final EuroService euroService;
     private final PizzaService pizzaService;
 
-    public PizzaController(EuroService euroService, PizzaService pizzaService) {
-        this.euroService = euroService;
+
+    public PizzaController(EuroService euroService, PizzaService pizzaService) {       this.euroService = euroService;
         this.pizzaService = pizzaService;
     }
+
+
 
     /*private final Pizza[] pizzas = {
             new Pizza(1, "Prosciutto", BigDecimal.valueOf(4), true),
@@ -87,5 +92,13 @@ public class PizzaController {
     @GetMapping("toevoegen/form")
     public ModelAndView toevoegenForm() {
         return new ModelAndView("toevoegen").addObject(new Pizza(0,"",null,false));
+    }
+    @PostMapping
+    public ModelAndView toevoegen(@Valid Pizza pizza, Errors errors){
+        if (errors.hasErrors()){
+            return new ModelAndView("toevoegen");
+        }
+        pizzaService.create(pizza);
+        return new ModelAndView("joskes","pizzas",pizzaService.findAll());
     }
 }
