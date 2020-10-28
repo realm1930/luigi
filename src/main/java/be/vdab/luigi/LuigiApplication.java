b09fc3c99b0014a222afcbe4211d5a6763308abd
Arne <a.vaneycken@gmail.com>
2020-10-28 17:29:22 +0100

    app installed

--- src/main/java/be/vdab/luigi/LuigiApplication.java
+++ src/main/java/be/vdab/luigi/LuigiApplication.java
@@ -2,12 +2,12 @@ package be.vdab.luigi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class LuigiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LuigiApplication.class, args);
     }
-
 }
